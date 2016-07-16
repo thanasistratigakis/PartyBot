e97cdda447dea053b7cfa2ee5a785cb47e827650
Thanasi Stratigakis <thanasi.strati@gmail.com>
2016-07-16 01:21:22 -0700

    spotify installed

---
+++ PartyBot-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PartyBot-Bridging-Header.h
+//  PartyBot
+//
+//  Created by Thanasi Stratigakis on 7/16/16.
+//  Copyright © 2016 PartyBot. All rights reserved.
+//
+
+#ifndef PartyBot_Bridging_Header_h
+#define PartyBot_Bridging_Header_h
+#import <Spotify/Spotify.h>
+
+#endif /* PartyBot_Bridging_Header_h */
